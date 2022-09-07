20e01c59d17c856cd4f14b026b9e5a6a892a43b2
Boris Reyes <b.reyesr@uniandes.edu.co>
2022-09-07 17:14:08 -0500

    Se importa la librería sweetAlert.js para manejar una alerta como notificación al perder la partida. Se crea la clase alertas.js donde se modela el comportamiento de la alerta.
    Verificar viablidad y mostrar cuando se pierda la partida.

---
+++ alertas.js
@@ -0,0 +1,14 @@
+
+(async () => {
+
+    await
+    Swal.fire({
+        icon: "warning",
+        title: "¡¡Oh no!!!",
+        text: "No cuentas con suficiete comida",
+        confirmButtonText: "Jugar de Nuevo",
+        allowOutsideClick: false
+    });
+
+}) ()
+
